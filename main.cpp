fd18e7722aa119268609eeb918651359be95d85a
okirich <kirillgren@gmail.com>
2015-08-16 18:16:29 +0400

    create linked list
    1st linked list

--- main.cpp
+++ main.cpp
@@ -2,40 +2,66 @@
  * File:   main.cpp
  * Author: Дом
  *
- * Created on 14 августа 2015 г., 22:10
+ * Created on 16 августа 2015 г., 11:59
  */
 
 #include <iostream>
+
 using namespace std;
-int i=0;
 
-//dynamic array
+struct Singly{
+    int data;
+    Singly *next;
+};
 
-void DynArray(){
-    int vol;
-    cout << "enter volume of array" <<endl;
-    cin >> vol;
-    cout << "volume is:" << vol <<endl;
-    int *p_DynArray = new int[vol];
-    cout << "enter values of elements" <<endl;
-    while(i<vol){
-        cout << i << ':' <<endl;
-        cin >> p_DynArray[i];
-        i++;
+Singly *head=0;
+
+void AddNode(int x){
+    Singly *Next=new Singly;
+    Next->data=x;
+    Next->next=0;
+    if (head==0){
+        head=Next;
+        return;
     }
-    cout << "Your array is:" <<endl;
-    for (int i=0; i<vol;i++)
-        cout << p_DynArray[i] <<',';
-    delete [] p_DynArray;
+    Singly *Cur=head;
+    while(Cur){
+        if(Cur->next==0){
+            Cur->next=Next;
+            return;
+        }
+        Cur=Cur->next;
+    }
 }
 
-//queue
-//stack
-//heap
-//hash func
+void display(){
+        Singly *Cur = head;
+        while(Cur){
+            cout<< Cur->data<<" ";
+            Cur=Cur->next;
+        }
+        cout<<endl;
+    }
 
-int main()
-{
-    DynArray();
+int main() {
+    int x;
+    while(x!=0){
+        cout<<"enter x"<<endl;
+        cout<<"1.AddNode"<<endl;
+        cout<<"2.Display"<<endl;
+        cout<<"0.exit"<<endl;
+        cin>>x;
+        switch(x){
+            case 1:
+                int a;
+                cout<<"enter data"<<endl;
+                cin>>a;
+                AddNode(a);
+                break;
+            case 2:
+                display();
+                break;
+        }
+    }
     return 0;
 }
